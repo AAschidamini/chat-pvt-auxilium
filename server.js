23523ef830835ba2631bb41f8d3e2f38b16cf94f
Andriele Aschidamini <andriele.aschidamini@pmweb.com.br>
2022-10-30 01:58:47 -0300

    ajuste de ambiente

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("teste");
+const express = require("express");
+
+const { resolve } = require("path");
+
+const app = express();
+
+app.use("/", express.static(resolve(__dirname, "./build")));
+
+app.listen(process.env.PORT || 3030, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
